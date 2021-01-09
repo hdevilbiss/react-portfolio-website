0772e1a356fbd8431e64440559a01859aaa3eef5
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-09 18:08:04 -0500

    🚚 pages: Rename AboutUs to Home, then add ServicesSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,14 @@
  * Components
  */
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
   return (
-    <AboutSection />
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
   )
 }
 
