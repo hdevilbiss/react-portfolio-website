a19cbb076733d2560dcab244f10ef1b7dedd8b86
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-09 18:36:27 -0500

    ❔ Home: Import FaqSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@
  */
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   )
 }
