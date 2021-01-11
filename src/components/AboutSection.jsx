ba0a3a42726e74a550bdfb538842901105747e44
hdevilbiss <hannahdevilbiss@gmail.com>
2021-01-10 21:52:09 -0500

    ✨ AboutSection: Create StyledAbout, Hide, & StyledImage styled components

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,40 +1,79 @@
 import home1 from '../images/incense.jpg';
+import styled from 'styled-components';
+
 const AboutSection = () => {
   return (
-    <div>
+    <StyledAbout>
       <div className="description">
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>
-              Our certified meditation coaches
+              We bring
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
-              will bring the <span>calm</span>
+              the <span>calm</span>
             </h2>
-          </div>
-          <div className="hide">
-          <h2>
-            to your workplace or event
-          </h2>
-          </div>
+          </Hide>
+          <Hide>
+            <h2>
+              to you
+            </h2>
+          </Hide>
         </div>
         <p>
-          Book a meditation event online. Choose a coach from our directory, or get paired based on your needs.
+          Book a meditation event online today.
         </p>
         <button>
           Book Now
         </button>
       </div>
-      <figure>
-        <img src={home1} alt="A person cooling some incense" />
-        <figcaption>
-          <span>Photo by <a href="https://unsplash.com/@conscious_design?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Conscious Design</a> on <a href="https://unsplash.com/s/photos/meditation?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-        </figcaption>
-      </figure>
-    </div>
+      <StyledImage>
+        <figure>
+          <img src={home1} alt="A person cooling some incense" />
+          <figcaption>
+            <span>Photo by <a href="https://unsplash.com/@conscious_design?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Conscious Design</a> on <a href="https://unsplash.com/s/photos/meditation?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+          </figcaption>
+        </figure>
+      </StyledImage>
+    </StyledAbout>
   )
 }
+
+/**
+ * Styled Component
+ */
+const StyledAbout = styled.div`
+  align-items: center;
+  background-color: #d8e2dc;
+  display: flex;
+  gap: 1rem;
+  justify-content: space-around;
+  min-height: 400px;
+  padding: 0rem 0rem 0rem 2rem;
+  .title {
+    letter-spacing: 0.25rem;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    //width: fit-content;
+  }
+  button {
+    width: 100%;
+  }
+  span {
+    color: #1D3461;
+    font-weight: bold;
+  }
+`;
+
+const StyledImage = styled.div`
+  overflow: hidden;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
