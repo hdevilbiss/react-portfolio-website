229ca6f1d84124026692ddf8c5792cb187b5544d
hdevilbiss <45151652+hdevilbiss@users.noreply.github.com>
2021-02-08 11:39:01 -0500

    💄 AboutSection: Add StyledDescription component

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const AboutSection = () => {
   return (
     <StyledAbout>
-      <div className="description">
+      <StyledDescription>
         <div className="title">
           <Hide>
             <h2>
@@ -13,7 +13,7 @@ const AboutSection = () => {
           </Hide>
           <Hide>
             <h2>
-              the <span>calm</span>
+              the <span className="calm">calm</span>
             </h2>
           </Hide>
           <Hide>
@@ -28,12 +28,12 @@ const AboutSection = () => {
         <button>
           Book Now
         </button>
-      </div>
+      </StyledDescription>
       <StyledImage>
         <figure>
           <img src={home1} alt="A person cooling some incense" />
           <figcaption>
-            <span>Photo by <a href="https://unsplash.com/@conscious_design?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Conscious Design</a> on <a href="https://unsplash.com/s/photos/meditation?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+            <span className="photo-credit">Photo by <a href="https://unsplash.com/@conscious_design?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Conscious Design</a> on <a href="https://unsplash.com/s/photos/meditation?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
           </figcaption>
         </figure>
       </StyledImage>
@@ -42,34 +42,67 @@ const AboutSection = () => {
 }
 
 /**
- * Styled Component
+ * Styled Components
  */
 const StyledAbout = styled.div`
   align-items: center;
-  background-color: #d8e2dc;
+  background-color: #111;
+  color: #eee;
   display: flex;
+  overflow: hidden;
   gap: 1rem;
-  justify-content: space-around;
-  min-height: 400px;
-  padding: 0rem 0rem 0rem 2rem;
+  justify-content: space-between;
+  min-height: 90vh;
+  padding: 0rem 2rem;
+  .calm {
+    color: #00afb9;
+    font-weight: bold;
+  }
   .title {
     letter-spacing: 0.25rem;
     margin-left: auto;
     margin-right: auto;
     text-align: center;
-    //width: fit-content;
+    width: fit-content;
+  }
+  .photo-credit {
+    font-style: italic;
+    a {
+      color:#00afb9;
+      transition: all 0.5s ease;
+      &:hover {
+        color: #fff;
+      }
+    }
+  }
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+  p {
+    text-align: center;
   }
   button {
-    width: 100%;
-  }
-  span {
-    color: #1D3461;
-    font-weight: bold;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 2rem;
+    padding-right: 2rem;
   }
 `;
 
 const StyledImage = styled.div`
+  flex: 1;
   overflow: hidden;
+  img {
+    height: 80vh;
+    object-fit: cover;
+    width: 100%;
+  }
 `;
 
 const Hide = styled.div`
