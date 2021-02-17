d47ce70b2517066b18590d0efeb513655b870f8b
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-17 15:15:07 -0500

    ✨ Services: Create Card & Cards styled divs.

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,9 +1,14 @@
 import { faCar, faCertificate, faSpa, faSmileBeam } from '@fortawesome/free-solid-svg-icons';
-import home2 from '../images/gong.jpg'
 import ServiceCard from './partials/ServiceCard';
+import home2 from '../images/gong.jpg'
+import styled from "styled-components";
+
+/**
+ * Return object
+ */
 const ServicesSection = () => {
   return (
-    <div className="services">
+    <Services>
       <div className="description">
         <h3>
           Equipment and Guidance
@@ -11,7 +16,7 @@ const ServicesSection = () => {
         <p>
           You need only bring yourself.
         </p>
-        <div className="cards">
+        <Cards>
           <ServiceCard
             icon={faCar}
             heading="Mobile"
@@ -29,10 +34,10 @@ const ServicesSection = () => {
           />
           <ServiceCard
             icon={faSmileBeam}
-            heading="Friendly, Five-Star Service"
+            heading="Five-Star Service"
             message="Our testimonials frequently mention the warm, friendly attitudes of our coaches."
           />
-        </div>
+        </Cards>
       </div>
       <figure>
         <img src={home2} alt="A person cooling some incense" />
@@ -40,8 +45,24 @@ const ServicesSection = () => {
         <span>Photo by <a href="https://unsplash.com/@madhatterzone?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Manja Vitolic</a> on <a href="https://unsplash.com/s/photos/gong?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
         </figcaption>
       </figure>
-    </div>
+    </Services>
   );
 }
+
+const Services = styled.div`
+  h3 {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+  p {
+    padding: 2rem 0rem 4rem 0rem;
+    width: 70%;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 export default ServicesSection;
