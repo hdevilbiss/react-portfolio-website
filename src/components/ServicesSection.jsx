3997e5e9333e29bbb5fd2b38d9cbd398a80af8fc
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-19 14:39:33 -0500

    🎨 Reformat Services. Add grid instead of flex.

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -2,6 +2,7 @@ import { faCar, faCertificate, faSpa, faSmileBeam } from '@fortawesome/free-soli
 import ServiceCard from './partials/ServiceCard';
 import home2 from '../images/gong.jpg'
 import styled from "styled-components";
+import { StyledImage } from "../styles";
 
 /**
  * Return object
@@ -11,26 +12,30 @@ const ServicesSection = () => {
     <Services>
       <div className="description">
         <h3>
-          Equipment and Guidance
+          You need only bring <span className="calm">yourself</span>
         </h3>
         <p>
-          You need only bring yourself.
+          We will bring equipment and guidance.
         </p>
+      </div>
+      <div className="grid">
         <Cards>
           <ServiceCard
             icon={faCar}
+            iconColor="#F00"
             heading="Mobile"
             message="We will commute to your location."
           />
           <ServiceCard
             icon={faCertificate}
+            iconColor="#FF0"
             heading="Certified"
             message="Well-educated coaches certified in meditation, yoga and wellness."
           />
           <ServiceCard
             icon={faSpa}
             heading="Relaxation"
-            message="We bring all the equipment needed, from incense to yoga mats, towels and candles, or hot stones and music."
+            message="We bring all the equipment needed, from incense and music, to towels and yoga mats, or hot stones and candles."
           />
           <ServiceCard
             icon={faSmileBeam}
@@ -38,49 +43,45 @@ const ServicesSection = () => {
             message="Our testimonials frequently mention the warm, friendly attitudes of our coaches."
           />
         </Cards>
-      </div>
-      <div className="flex-image">
-        <figure>
-          <img src={home2} alt="A person behind a gong using a foam roller and yoga mat" />
-          <figcaption>
-          <span>Photo by <a href="https://unsplash.com/@madhatterzone?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Manja Vitolic</a> on <a href="https://unsplash.com/s/photos/gong?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-          </figcaption>
-        </figure>
+        <StyledImage>
+          <figure>
+            <img src={home2} alt="A person behind a gong using a foam roller and yoga mat" />
+            <figcaption>
+            <span>Photo by <a href="https://unsplash.com/@madhatterzone?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Manja Vitolic</a> on <a href="https://unsplash.com/s/photos/gong?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+            </figcaption>
+          </figure>
+        </StyledImage>
       </div>
     </Services>
   );
 }
 
 const Services = styled.div`
-  align-items: center;
-  display: flex;
-  overflow: hidden;
-  gap: 1rem;
-  justify-content: space-between;
   margin-left: auto;
   margin-right: auto;
-  padding-left: 1rem;
-  padding-top: 2rem;
-  .flex-image {
-    flex: 1;
-    overflow: hidden;
-    img {
-      height: 80vh;
-      object-fit: cover;
-      width: 100%;
-    }
+  min-height: 90vh;
+  padding: 1rem 2rem;
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
   @media only screen and (max-width: 640px) {
+    padding-bottom: 1rem;
     padding-top: 1rem;
-    .flex-image {
-      display: none;
+    .grid {
+      grid-template-columns: repeat(1, minmax(0, 1fr));
     }
   }
 `;
 
 const Cards = styled.div`
+  align-items: center;
   display: flex;
   flex-wrap: wrap;
+  overflow: hidden;
+  gap: 1rem;
+  justify-content: space-between;
+
 `;
 
 export default ServicesSection;
--- src/components/partials/ServiceCard.jsx
+++ src/components/partials/ServiceCard.jsx
@@ -4,32 +4,41 @@ import styled from "styled-components";
 /**
  * Return object
  */
-const ServiceCard = ({icon, heading, message}) => {
+const ServiceCard = ({icon, iconColor, heading, message}) => {
   return (
     <Card>
-      <div className="icon">
-        <FontAwesomeIcon icon={icon} size="2x" />
-        <h4>
-          {heading}
-        </h4>
-      </div>
-      <p>
-        {message}
-      </p>
+      <section>
+        <header>
+          <div className="icon" iconColor={iconColor}>
+            <FontAwesomeIcon icon={icon} size="2x" />
+          </div>
+          <h4>
+            {heading}
+          </h4>
+        </header>
+        <p>
+          {message}
+        </p>
+      </section>
     </Card>
   );
 }
 
 const Card = styled.div`
-  border: 1px solid #000;
   flex-basis: 15rem;
+  margin-left: auto;
+  margin-right: auto;
   h4 {
     margin-left: 1rem;
-    padding: 1rem;
   }
-  .icon {
+  header {
     align-items: center;
     display: flex;
+  }
+  .icon {
+    svg {
+      color: ${props => props.iconColor ? props.iconColor : "inherit"};
+    }
   }
 `;
 
