fc2ecfbc24f855159d33fd213b3ed3d276888bfe
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-21 16:47:15 -0500

    🚧 Add image credit to ContactUs

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,6 +4,10 @@ const ContactUs = () => {
       <h2>
         Contact Us
       </h2>
+      <h3>
+        Person With Eyes Closed
+      </h3>
+      <span>Photo by <a href="https://unsplash.com/@motoki?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Motoki Tonn</a> on <a href="https://unsplash.com/s/photos/meditation?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
     </div>
   );
 }
