926ae2b92ab6b2f972f64383c19be4375c9e5c50
hdevilbiss <45151652+hdevilbiss@users.noreply.github.com>
2021-02-27 20:36:06 -0500

    💫 Wrap Switch in AnimatePresence with prop exitBeforeEnter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import {Switch, Route} from "react-router-dom";
-
+import { Switch, Route, useLocation } from "react-router-dom";
 /**
  * Pages
  */
@@ -8,31 +7,37 @@ import Nav from "./components/Nav";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
 import SingleWork from "./pages/SingleWork";
-
 /**
  * Styles
  */
 import GlobalStyle from './components/GlobalStyle';
+/**
+ * Animations
+ */
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <SingleWork />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <SingleWork />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
