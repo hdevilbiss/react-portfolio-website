4ede87cbb33bb809615f0003896854264f00a06d
Hannah <45151652+shed-00@users.noreply.github.com>
2021-03-03 17:28:08 -0500

    💥 Wrapping Sections in AnimatedPage breaks layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,15 +9,18 @@ import FaqSection from '../components/FaqSection';
  */
 import { motion, useReducedMotion } from "framer-motion";
 import { pageAnimation, accessiblePageAnimation } from "../animation";
+import AnimatedPage from "../components/animations/AnimatedPage";
 
 const Home = () => {
   const shouldReduceMotion = useReducedMotion();
   return (
-    <motion.div variants={shouldReduceMotion ? accessiblePageAnimation : pageAnimation} initial="hidden" animate="show" exit="exit">
-        <AboutSection />
-        <ServicesSection />
-        <FaqSection />
-    </motion.div>
+    // <motion.div variants={shouldReduceMotion ? accessiblePageAnimation : pageAnimation} initial="hidden" animate="show" exit="exit">
+        <AnimatedPage>
+          <AboutSection />
+          <ServicesSection />
+          <FaqSection />
+        </AnimatedPage>
+    // </motion.div>
   )
 }
 
