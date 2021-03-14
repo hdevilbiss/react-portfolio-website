12d490b44629a70b1941810ef45884788603176c
Hannah <45151652+shed-00@users.noreply.github.com>
2021-03-14 14:29:22 -0400

    🚚 Import FAQ from faq.js then map in FaqSection

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,71 +1,91 @@
+import { useState } from "react";
+/** Components */
 import QuestionAnswer from './partials/QuestionAnswer';
+
+/** Style */
 import styled from "styled-components";
 import { Section } from "../styles";
 
+/** Data */
+import getFAQ from "../faq";
+
 const FaqSection = () => {
+  const dataForFAQ = getFAQ();
+
   return (
     <StyledFaq>
       <section>
       <header>
-        <h3>
-          Any questions?
-        </h3>
-        <h4 className="calm">
+        <h3 className="calm">
           FAQ
+        </h3>
+        <h4>
+          Any questions?
         </h4>
       </header>
-      <QuestionAnswer
-        question="What's the first step?"
-        answer="Fill out a form to assess your needs, then optionally make an account to book your first event."
-      />
-      <QuestionAnswer
-        question="Why should I hire one of your coaches?"
-        answer="Having a coach can provide a more personalized experience to your meditation or yoga practice, able to provide relevant cues and advice to fit your needs."
-      />
-      <QuestionAnswer
-        question="What is the benefit of meditation or yoga?"
-        answer="Meditation and yoga can have an immediate effect on your body, reducing blood pressure, improving your mood, and bringing flexibility to stiff muscles."
-      />
-      <QuestionAnswer
-        question="Does this require a commitment or membership?"
-        answer="Nope! You can try our services once, or schedule a recurring event. It's in your hands."
-      />
-      <QuestionAnswer
-        question="How much does it cost? What payment methods do you accept?"
-        answer="Visit the Pricing page to learn more."
-      />
-      <QuestionAnswer
-        question="What if something goes wrong?"
-        answer="We take customer satisfaction seriously. If something didn't go right during your event, please contact our team at hello@example.com."
-      />
-      <QuestionAnswer
-        question="How do I cancel an event?"
-        answer="You can cancel an event up to 24 hours in advance without penalty by logging into your account, selecting 'My Events', and selecting 'Cancel this event'. Events cancelled within 24 hours of the event will only receive a partial refund. And don't worry - our coaches are guaranteed to be paid even if you must cancel last-minute."
-      />
+        <div className="qa">
+        {dataForFAQ.map(({id, question, answer}) => {
+          return (
+            <QuestionAnswer
+              key={id}
+              question={question}
+              answer={answer}
+            />
+          )
+        })}
+        </div>
       </section>
     </StyledFaq>
   );
 }
 
 const StyledFaq = styled(Section)`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 65ch;
   header {
     margin-left: auto;
     margin-right: auto;
+    padding-left: 4rem;
+  }
+  h3 {
+    font-size: 5rem;
+    font-weight: bolder;
+  }
+  h5, p {
+    font-size: 1.2rem;
+  }
+  h5:first-child {
+    padding-top: 2rem;
+  }
+  img {
+    height: 70vh;
+    object-fit: cover;
+  }
+  .qa {
+    background: #EEE;
+    border-radius: 5px;
+    color: #111;
+    margin: 0;
   }
   .question {
     cursor: pointer;
-    padding-left: 3rem;
   }
-  .answer {
-    padding-left: 4rem;
+  .question, .answer {
+    padding: 0 3rem;
   }
   @media only screen and (max-width: 640px) {
-      .question {
-        padding: 1rem;
-      }
-      .answer {
-        padding: 1.5rem;
-      }
+    padding: 0rem;
+    header {
+      padding-left: 0rem;
+      text-align: center;
+    }
+    .question {
+      padding: 0.5rem;
+    }
+    .answer {
+      padding: 0.5rem;
+    }
   }
 `;
 
