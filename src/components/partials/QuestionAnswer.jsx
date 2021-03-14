39a85f93aad1c4e9d3b65a9f6c4d8b1a79944958
Hannah <45151652+shed-00@users.noreply.github.com>
2021-03-14 14:29:59 -0400

    ✨ Toggle answers on / off in QuestionAnswer

--- src/components/partials/QuestionAnswer.jsx
+++ src/components/partials/QuestionAnswer.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+import Toggle from "./Toggle";
+
 const QuestionAnswer = ({question, answer}) => {
+  /** State */
+  const [faqToggle, setFaqToggle] = useState(false);
+
+  /** Events */
+  const questionClickHandler = () => {
+    setFaqToggle(!faqToggle);
+  }
+
   return (
     <div>
-      <div className="question">
+      <div onClick={questionClickHandler} className="question">
         <h5>
           {question}
         </h5>
       </div>
-      <div className="answer">
-        <p>
-          {answer}
-        </p>
-      </div>
+      {faqToggle && (
+        <div className="answer">
+          <p>
+            {answer}
+          </p>
+        </div>
+      )}
       <div className="line"></div>
     </div>
   );
